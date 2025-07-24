bafa3cce0f8a0ec4d190319829f886ad3a044b03
number571 <number571@example.com>
2025-07-24 14:05:56 +0700

    update

--- internal/app/init_app.go
+++ internal/app/init_app.go
@@ -14,7 +14,7 @@ func InitApp(pArgs []string, pFlags flag.IFlags) (types.IRunner, error) {
 	inputPath := strings.TrimSuffix(pFlags.Get("-p").GetStringValue(pArgs), "/")
 	_, err := build.SetBuildByPath(inputPath)
 	if err != nil {
-		return nil, errors.Join(ErrSetNetworks, err)
+		return nil, errors.Join(ErrSetBuild, err)
 	}
 	networkKey := pFlags.Get("-n").GetStringValue(pArgs)
 	return NewApp(networkKey, inputPath), nil
